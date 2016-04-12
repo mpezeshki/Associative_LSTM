95d9df73aa484a36c5091cc08c6d68274cc2fd1d
Mohammad Pezeshki <mohammadpz@gmail.com>
2016-04-12 09:52:46 -0400

    VOILA! permutation is now there.

--- holographic_memory.py
+++ holographic_memory.py
@@ -5,6 +5,31 @@ import theano.tensor as T
 # matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+B = 10
+F = 110 * 110 * 3
+C = 20
+
+# shape: C x F/2
+permutations = []
+indices = np.arange(F / 2)
+for i in range(C):
+    np.random.shuffle(indices)
+    permutations.append(np.concatenate(
+        [indices,
+         [ind + F / 2 for ind in indices]]))
+# C x F (numpy)
+PERMUTATIONS = np.vstack(permutations)
+
+
+# input: B x F
+# output: C x B x F
+def permute(input):
+    inputs_permuted = []
+    for i in range(PERMUTATIONS.shape[0]):
+        inputs_permuted.append(
+            input[:, PERMUTATIONS[i]].dimshuffle('x', 0, 1))
+    return T.concatenate(inputs_permuted, axis=0)
+
 
 # shape: C x B x F (F=2n)
 def complex_mult(r, u, inverse_r=False, moduli_1=False):
@@ -30,7 +55,7 @@ def complex_mult(r, u, inverse_r=False, moduli_1=False):
 # mem: C x F
 def read(key, mem):
     value = complex_mult(
-        key,
+        permute(key),
         mem.dimshuffle(0, 'x', 1),
         inverse_r=True, moduli_1=True)
     return value.mean(axis=0)
@@ -40,42 +65,41 @@ def read(key, mem):
 # value: B x F
 # mem: C x F
 def write(key, value):
-    coded_value = complex_mult(key, value.dimshuffle('x', 0, 1))
+    coded_value = complex_mult(permute(key), value.dimshuffle('x', 0, 1))
     return coded_value.sum(axis=1)
 
-key = T.tensor3('key')
+# B x F
+key = T.matrix('key')
+# B x F
 value = T.matrix('value')
+# C x F
 mem = T.matrix('mem')
 
 read_func = theano.function([key, mem], read(key, mem))
 write_func = theano.function([key, value], write(key, value))
 
-
-# Let's test it!
-B = 5
-F = 110 * 110 * 3
-C = 20
-
 # shape: 20 x 110 x 110 x 3
 data = np.load('20_images_from_imagenet.npy')[:B]
-VALUES = data.reshape(B, F) - 0.5
+VALUES = data.reshape(B, F) - np.mean(data.reshape(B, F),
+                                      axis=1, keepdims=True)
 
-phis = np.random.random((C, B, F / 2)) * 2 * np.pi
-KEYS = np.concatenate([np.cos(phis), np.sin(phis)], axis=2)
+phis = np.random.random((B, F / 2)) * 2 * np.pi
+KEYS = np.concatenate([np.cos(phis), np.sin(phis)], axis=1)
 
 MEM = write_func(KEYS, VALUES)
+
 all_imgs = read_func(KEYS, MEM)
 
-data = VALUES.reshape(B, 110, 110, 3)
-data = np.swapaxes(data, 0, 1)
-data = np.reshape(data, (110, 110 * B, 3))
-plt.imshow(data[:, :110 * B] + 0.5)
+VALUES = VALUES + np.mean(data.reshape(B, F), axis=1, keepdims=True)
+VALUES = VALUES.reshape(B, 110, 110, 3)
+VALUES = np.swapaxes(VALUES, 0, 1)
+VALUES = np.reshape(VALUES, (110, 110 * B, 3))
+plt.imshow(VALUES[:, :110 * B])
 plt.show()
 
+all_imgs = all_imgs + np.mean(data.reshape(B, F), axis=1, keepdims=True)
 all_imgs = all_imgs.reshape(B, 110, 110, 3)
 all_imgs = np.swapaxes(all_imgs, 0, 1)
 all_imgs = np.reshape(all_imgs, (110, 110 * B, 3))
-plt.imshow(all_imgs[:, :110 * B] + 0.5)
+plt.imshow(all_imgs[:, :110 * B])
 plt.show()
-
-import ipdb; ipdb.set_trace()
